f315d4a909715d3dc8725cbdf710b9b1f729e3c7
max <mburger89@me.com>
2016-03-26 14:58:45 -0700

    p.38

--- collection/views.py
+++ collection/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+	# definig the variable
+	number = 6
 	#this is your new view
-	return render(request, 'index.html')
+	return render(request, 'index.html', {
+		'number': number,
+	})
--- hellowebapp/settings.py
+++ hellowebapp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'hellowebapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "hellowebapp/templates")],
+        'DIRS': [os.path.join(BASE_DIR, "collection/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
